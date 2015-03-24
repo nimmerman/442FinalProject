9cd605ec840854dac4cad56387fb291042b1a72c
Alexander Chocron <achocron@0587360243.wireless.umich.net>
2015-03-24 12:28:24 -0400

    "fixed some issues"

--- findNormals.m
+++ findNormals.m
@@ -1,4 +1,4 @@
-function [ M ] = findNormals(parted_bounds, num_partitions, source_points)
+function [ Norms ] = findNormals(parted_bounds, num_partitions, source_points)
 %FINDNORMALS - This function finds the normals of points on a given
 %boundary
 %INPUT - parted_bounds
@@ -6,38 +6,52 @@ function [ M ] = findNormals(parted_bounds, num_partitions, source_points)
 %        source_points: a cell array of 3*num_partitions x 2 matrix of points to use for
 %        an estimate of each patch
 
-
+Norms = cell(length(parted_bounds),1);
 for h = 1:length(parted_bounds)
     
     bound_h = parted_bounds{h};
-    M = zeros(size(part,1),size(part,2)+1);
-
+    
+    M = zeros(size(bound_h,1),size(bound_h,2)+1);
+    if source_points
+        points = source_points{h};
+    else
+        points = 0; %for testing
+    end
     for i = 1:num_partitions
-        part = bound_h{i};
-        points_i = source_points{i};
-                
-        p1 = points_i(3*i - 2);
-        p2 = points_i(3*i - 1);
-        p3 = points_i(3*i);
+        if (points)
+            points_i = points((3*i - 2):3*i);
+        else
+            points_i = [4 2 1; 1 4 2]'; %for testing
+        end
+        p1 = points_i(1,:);
+        p2 = points_i(2,:);
+        p3 = points_i(3,:);
         
         x = [p1(1) p2(1) p3(1)];
         y = [p1(2) p2(2) p3(2)];
         
-        [a,~,~] = polyfit(x,y,2);
-        
+        [coefs,~,~] = polyfit(x,y,2);
+        a = coefs(1);
         c_x = 2*i - 1;
         c_y = 2*i;
+        size(bound_h,2)
+        points_per_part = size(bound_h,1)/num_partitions;
+        r0 = (i - 1)*points_per_part;
+        
+
         %might need to convert to x and y first
-        for j = 1:(size(M,1)/num_partitions)
-            point = part(j,c_x:c_y);
+        for j = 1:points_per_part
+            point = bound_h(j,c_x:c_y);
             Nx = -2*a*point(1)/sqrt(1+4*a^2*point(1)^2);
-            Ny = 1/sqrt(1+4*a^2*point(1)^2);
-            
-            M(j,c_x) = Nx;
-            M(j,c_y) = Ny;
+            Ny = sqrt(1 - Nx^2);
+            M(r0+j,c_x) = Nx;
+            M(r0+j,c_y) = Ny;
         end 
     end
+    M(:,end) = 1;
+    Norms{h} = M;
+    
 end
 
-M(:,end) = 1;
+
 end
